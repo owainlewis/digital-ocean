a00397ae93317f5d8f81dae3325df921f49b8e76
owain lewis <owain@owainlewis.com>
2014-02-01 15:48:58 +0000

    finish sizes

--- src/digitalocean/sizes.clj
+++ src/digitalocean/sizes.clj
@@ -1,2 +1,7 @@
 (ns digitalocean.sizes
   (require [digitalocean.core :as core]))
+
+(defn sizes
+  "This method returns all the available sizes that can be used to create a droplet"
+  ([client-id api-key]
+     (core/get-for "sizes" client-id api-key)))
