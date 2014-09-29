bd54720c41d74bcb56059975c816cff0c48cfe44
Owain Lewis <owain@owainlewis.com>
2014-09-29 12:30:17 +0100

    Update events_test.clj

--- test/digitalocean/v1/events_test.clj
+++ test/digitalocean/v1/events_test.clj
@@ -1,13 +1,8 @@
 (ns digitalocean.v1.events-test
   (:use midje.sweet)
   (:require [digitalocean.v1.events :as doe])
-  (:require [digitalocean.v1.core :as doc])
-  )
-
+  (:require [digitalocean.v1.core :as doc]))
 
 (facts "about event"
   (doe/event "FOO" "BAR" "1234") =>  "event-info"
   (provided (doc/request "events/1234" "FOO" "BAR"  ) => {:event "event-info"}))
-
-
-
