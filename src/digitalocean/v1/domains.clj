8b66637e46c747006a90ab579250c67eb507c93e
Owain Lewis <owain@owainlewis.com>
2016-03-15 09:44:53 +0000

    Update domains.clj

--- src/digitalocean/v1/domains.clj
+++ src/digitalocean/v1/domains.clj
@@ -1,14 +1,11 @@
 (ns digitalocean.v1.domains
   (require [digitalocean.v1.core :as core]))
 
-;; GET /domains
-
 (defn domains
   "This method returns all of your current domains"
   [client-id api-key]
   (core/get-for "domains" client-id api-key))
 
-;; GET /domains/new
 (defn new-domain
   "This method creates a new domain name with an A record for the specified [ip_address]
    - name Required, string, name of the domain.
@@ -18,8 +15,6 @@
     (core/request "domains/new" client-id api-key
       domain-params)))
 
-;; GET /domains/[domain_id]
-
 (defn domain
   "This method returns the specified domain
    - domain_id Required, Integer or Domain Name (e.g. domain.com), specifies the domain to display"
@@ -28,13 +23,8 @@
     (->> response :domain)))
 
 ;; GET /domains/[domain_id]/destroy
-
 ;; GET /domains/[domain_id]/records
-
 ;; GET /domains/[domain_id]/records/new
-
 ;; GET /domains/[domain_id]/records/[record_id]
-
 ;; GET /domains/[domain_id]/records/[record_id]/edit
-
 ;; GET /domains/[domain_id]/records/[record_id]/destroy
